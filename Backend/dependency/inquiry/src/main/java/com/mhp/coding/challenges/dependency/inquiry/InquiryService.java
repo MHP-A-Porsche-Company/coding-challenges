02746ef08f3720f93e3099f2b71cbbec16d941a3
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 12:32:43 +0200

    Remove Observer pattern. Service now publishes InquiryEvent

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,20 +1,23 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
-import java.util.Observable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
-public class InquiryService extends Observable {
+public class InquiryService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
+	private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
+
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
 
     public void create(final Inquiry inquiry) {
-        LOG.info("User sent inquiry: {}", inquiry);
-		setChanged();
-		notifyObservers(inquiry);
-    }
+		InquiryEvent inquiryEvent = new InquiryEvent(this, inquiry);
+		applicationEventPublisher.publishEvent(inquiryEvent);
 
+        LOG.info("User sent inquiry: {}", inquiry);
+    }
 }
