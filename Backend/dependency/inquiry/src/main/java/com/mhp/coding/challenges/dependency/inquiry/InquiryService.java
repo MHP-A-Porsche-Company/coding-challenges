2397888c129ced8bc7491c2000f874668dc2f4db
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 11:17:47 +0200

    Make InquiryService observable, notify Observers in create method

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,16 +1,20 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import java.util.Observable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class InquiryService {
+public class InquiryService extends Observable {
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
 
     public void create(final Inquiry inquiry) {
         LOG.info("User sent inquiry: {}", inquiry);
+		setChanged();
+		notifyObservers(inquiry);
     }
 
 }
