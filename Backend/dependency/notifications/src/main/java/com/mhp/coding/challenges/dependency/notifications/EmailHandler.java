c361cd95cc9f2f805d3eba52de6e7d7360dcc18f
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 11:19:04 +0200

    Implement Observer Interface with EmailHandler. Add postconstruct and update methods. Inject and observe InquiryService

--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
@@ -1,17 +1,39 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
-import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryService;
+
 @Component
-public class EmailHandler {
+public class EmailHandler implements Observer {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmailHandler.class);
 
-    public void sendEmail(final Inquiry inquiry) {
-        LOG.info("Sending email for: {}", inquiry);
+	@Autowired
+	InquiryService inquiryService;
+
+	@PostConstruct
+	public void init() {
+		inquiryService.addObserver(this);
+		LOG.info("EmailHandler is observing InquiryService");
+	}
+
+	public void sendEmail(Inquiry inquiry) {
+		LOG.info("Sending email for: {}", inquiry);
     }
+
+	@Override
+	public void update(Observable o, Object inquiry) {
+		this.sendEmail((Inquiry) inquiry);
+	}
 
 }
