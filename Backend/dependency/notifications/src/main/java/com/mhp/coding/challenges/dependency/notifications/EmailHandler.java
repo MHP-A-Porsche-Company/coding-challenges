58d4daf0f39c667e40882c17dbd5846a35c5cbd8
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 12:33:35 +0200

    Remove Observer Pattern from EmailHandler. Class now implements Spring ApplicationListener Interface

--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/EmailHandler.java
@@ -1,39 +1,29 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
-import java.util.Observable;
-import java.util.Observer;
-
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryEvent;
 import com.mhp.coding.challenges.dependency.inquiry.InquiryService;
 
 @Component
-public class EmailHandler implements Observer {
+public class EmailHandler implements ApplicationListener<InquiryEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmailHandler.class);
 
 	@Autowired
 	InquiryService inquiryService;
 
-	@PostConstruct
-	public void init() {
-		inquiryService.addObserver(this);
-		LOG.info("EmailHandler is observing InquiryService");
-	}
-
 	public void sendEmail(Inquiry inquiry) {
 		LOG.info("Sending email for: {}", inquiry);
     }
 
 	@Override
-	public void update(Observable o, Object inquiry) {
-		this.sendEmail((Inquiry) inquiry);
+	public void onApplicationEvent(InquiryEvent inquiryEvent) {
+		sendEmail(inquiryEvent.getInquiry());
 	}
-
 }
