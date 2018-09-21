488492b89b6c78b38c2c54b0fa5d92e0818de6c5
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 11:19:17 +0200

    Implement Observer Interface with PushNotificationHandler. Add postconstruct and update methods. Inject and observe InquiryService

--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
@@ -1,16 +1,39 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
-import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryService;
+
 @Component
-public class PushNotificationHandler {
+public class PushNotificationHandler implements Observer {
 
     private static final Logger LOG = LoggerFactory.getLogger(PushNotificationHandler.class);
 
-    public void sendNotification(final Inquiry inquiry) {
-        LOG.info("Sending push notification for: {}", inquiry);
+	@Autowired
+	InquiryService inquiryService;
+
+	@PostConstruct
+	public void init() {
+		inquiryService.addObserver(this);
+		LOG.info("PushNotificationHandler is observing InquiryService");
+	}
+
+	public void sendNotification(final Inquiry inquiry) {
+		LOG.info("Sending push notification for: {}", inquiry);
     }
+
+	@Override
+	public void update(Observable o, Object inquiry) {
+		this.sendNotification((Inquiry) inquiry);
+	}
+
 }
