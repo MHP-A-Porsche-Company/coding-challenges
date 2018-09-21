b9b568b119c391f2f3f38a543d770070d18d5d22
Rodney Mitchell <rodney.mitchell@outlook.com>
2018-09-21 12:33:48 +0200

    Remove Observer Pattern from PushNotificationHandler. Class now implements Spring ApplicationListener Interface

--- Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
+++ Backend/dependency/notifications/src/main/java/com/mhp/coding/challenges/dependency/notifications/PushNotificationHandler.java
@@ -1,39 +1,29 @@
 package com.mhp.coding.challenges.dependency.notifications;
 
-import java.util.Observable;
-import java.util.Observer;
-
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.mhp.coding.challenges.dependency.inquiry.Inquiry;
+import com.mhp.coding.challenges.dependency.inquiry.InquiryEvent;
 import com.mhp.coding.challenges.dependency.inquiry.InquiryService;
 
 @Component
-public class PushNotificationHandler implements Observer {
+public class PushNotificationHandler implements ApplicationListener<InquiryEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(PushNotificationHandler.class);
 
 	@Autowired
 	InquiryService inquiryService;
 
-	@PostConstruct
-	public void init() {
-		inquiryService.addObserver(this);
-		LOG.info("PushNotificationHandler is observing InquiryService");
-	}
-
 	public void sendNotification(final Inquiry inquiry) {
 		LOG.info("Sending push notification for: {}", inquiry);
     }
 
 	@Override
-	public void update(Observable o, Object inquiry) {
-		this.sendNotification((Inquiry) inquiry);
+	public void onApplicationEvent(InquiryEvent inquiryEvent) {
+		sendNotification(inquiryEvent.getInquiry());
 	}
-
 }
