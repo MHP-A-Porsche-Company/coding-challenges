1de2fe226ff1dec073ba603b9930e4d91ba7749e
Saket Saurav <saket.saurav@seamless.se>
2018-11-21 20:29:31 +0530

    code-challenge test code submission-- Saket Saurav

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,7 +1,10 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import com.mhp.coding.challenges.dependency.notifications.EmailHandler;
+import com.mhp.coding.challenges.dependency.notifications.PushNotificationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +12,21 @@ public class InquiryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
 
+    private Inquiry inquiry;
+
+    @Autowired
+    private EmailHandler emailHandler;
+
+    @Autowired
+    private PushNotificationHandler pushNotificationHandler;
+
     public void create(final Inquiry inquiry) {
+        this.inquiry = inquiry;
         LOG.info("User sent inquiry: {}", inquiry);
+        emailHandler.sendEmail(inquiry);
+        pushNotificationHandler.sendNotification(inquiry);
     }
+
+
 
 }
